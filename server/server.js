0271155a112882c5b6be986f3e47c00e3897428f
LuisSosa97 <wwicho7@hotmail.com>
2020-04-23 01:21:47 -0500

    first commit

--- server/server.js
+++ server/server.js
@@ -4,20 +4,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//HABILITACION DE CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Origin, x-Request-With, Content-Type,Accept, Authorization, token'
-
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, token'
     );
-
     res.setHeader(
         'Access-Control-Allow-Methods',
-        'GET, POST, DELETE,OPTIONS'
+        'GET, POST, PUT, DELETE, OPTIONS'
     );
     next();
-});
+})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -30,10 +29,24 @@ app.use(require('./routes/login'));
 app.use(require('./routes/image'));
 //app.use(require('./models/prestamo'));
 
-mongoose.connect('mongodb+srv://admin:140797@cluster0-tsywd.mongodb.net/bibloteca?retryWrites=true&w=majority', (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos ONLINE');
-});
+// mongoose.connect('mongodb+srv://admin:140797@cluster0-tsywd.mongodb.net/bibloteca?retryWrites=true&w=majority', (err, res) => {
+//     if (err) throw err;
+//     console.log('Base de datos ONLINE');
+// });
+// app.listen(process.env.PORT, () => {
+//     console.log('Escuchando el puerto 3000');
+// });
+
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
+    });
+
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto 3000');
+    console.log('Escuchando por el puerto: 3000');
 });
